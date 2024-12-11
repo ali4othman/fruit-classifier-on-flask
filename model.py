06b03f62b384ca62a754a567e117353cbaa9ed15
Ali Othman <107825557+ali4othman@users.noreply.github.com>
2024-12-12 00:57:31 +0200

    CNN modified

--- model.py
+++ model.py
@@ -33,20 +33,20 @@ classes = os.listdir(os.path.join(directory, 'train'))
 
 model = Sequential([
     Conv2D(32, (3,3), activation = 'relu', input_shape = (256,256,3)),
-    MaxPooling2D((2,2)),
     BatchNormalization(),
+    MaxPooling2D((2,2)),
 
     Conv2D(64, (3,3), activation = 'relu'),
-    MaxPooling2D((2,2)),
     BatchNormalization(),
+    MaxPooling2D((2,2)),
 
     Conv2D(128, (3,3), activation = 'relu'),
-    MaxPooling2D((2,2)),
     BatchNormalization(),
+    MaxPooling2D((2,2)),
 
     Conv2D(256, (3,3), activation = 'relu'),
-    MaxPooling2D((2,2)),
     BatchNormalization(),
+    MaxPooling2D((2,2)),
 
     Flatten(),
     Dense(512),
